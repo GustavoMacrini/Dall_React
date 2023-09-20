3774f039aed5e31111a5777b7c429bf5bc73da08
GustavoMacrini <gustavo.godencio@hotmail.com>
2023-09-19 23:04:33 -0300

    Header - Arrumando fixação navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,41 +5,39 @@ import Logo from '../icons/logo.svg';
 import Menu from '../icons/menu.svg';
 import CloseMenu from '../icons/close-menu.svg';
 import { Link } from 'react-router-dom';
-import './header.css'
 import React, { useState } from 'react';
-
-
+import './header.css';
 
 function Header() {
   const [menu, setMenu] = useState(true);
 
   return (
-    <Navbar expand="lg" className="navbar bg-body-tertiary">
-      <Container className=''>
-        <Navbar.Brand href="#home">
-          <Link to='/'>
-            <img src={Logo}/>
-          </Link>
-        </Navbar.Brand>
+    <div className='navbar-wrapper'>
+      <Navbar expand="lg" className="bg-body-tertiary navbar">
+        <Container className=''>
+          <Navbar.Brand href="#home">
+            <Link to='/'>
+              <img src={Logo}/>
+            </Link>
+          </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setMenu(!menu)}> 
-          <img src={menu? Menu : CloseMenu}/>
-        </Navbar.Toggle>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setMenu(!menu)}> 
+            <img src={menu? Menu : CloseMenu}/>
+          </Navbar.Toggle>
+          
+          <Navbar.Collapse className="justify-content-end">
+            <Nav className="navbar-items" id='teste'>
+              <Nav.Link as={Link} to='/'>INÍCIO</Nav.Link>
+              <Nav.Link as={Link} to='/produtos'>NOSSOS PRODUTOS</Nav.Link>
+              <Nav.Link as={Link} to='/sobre'>SOBRE NÓS</Nav.Link>
+              <Nav.Link as={Link} to='/contato'>CONTATO</Nav.Link>
+              <Nav.Link as={Link} to='/politica'>POLÍTICAS DE PRIVACIDADE</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
-
-        
-        <Navbar.Collapse className="justify-content-end">
-          <Nav className="navbar-items" id='teste'>
-            <Nav.Link as={Link} to='/'>INÍCIO</Nav.Link>
-            <Nav.Link as={Link} to='/produtos'>NOSSOS PRODUTOS</Nav.Link>
-            <Nav.Link as={Link} to='/sobre'>SOBRE NÓS</Nav.Link>
-            <Nav.Link as={Link} to='/contato'>CONTATO</Nav.Link>
-            <Nav.Link as={Link} to='/politica'>POLÍTICAS DE PRIVACIDADE</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
